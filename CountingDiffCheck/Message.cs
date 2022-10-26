c6c6b070d914a227a8676a095d05f8fa48a809ef
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-10-26 20:27:53 +0700

    Checking for username change

---
+++ CountingDiffCheck/Message.cs
@@ -0,0 +1,125 @@
+﻿using System.Globalization;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace CountingDiffCheck.Model;
+public partial class Message : ObservableObject
+{
+    [ObservableProperty]
+    private User sender = new();
+
+    [ObservableProperty]
+    private DateTime sendAt;
+
+    [ObservableProperty]
+    private string? content;
+
+    [ObservableProperty]
+    private string? attachments;
+}
+
+public partial class User : ObservableObject
+{
+    public User()
+    {
+        UserID = long.MinValue;
+        UserName = string.Empty;
+    }
+
+    [ObservableProperty]
+    private long userID;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Name))]
+    [NotifyPropertyChangedFor(nameof(Discriminator))]
+    string userName = string.Empty;
+
+    public string Name
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(UserName) || !UserName.Contains('#'))
+            {
+                return string.Empty;
+            }
+
+            var span = UserName.AsSpan();
+            return new string(span[..^5]);
+        }
+    }
+
+    public int Discriminator
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(UserName) || !UserName.Contains('#'))
+            {
+                return -1;
+            }
+
+            var span = UserName.AsSpan();
+            var succeed = int.TryParse(span[^4..], out var result);
+            return succeed ? result : -1;
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not User)
+            return false;
+
+        if (obj is null)
+            return false;
+
+        if (obj is User compare && UserID == compare.UserID)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
+
+public class MessageMapper : ClassMap<Message>
+{
+    public MessageMapper()
+    {
+        Map(m => m.Sender.UserName).Name("Author");
+        Map(m => m.Sender.UserID).Name("AuthorID");
+        Map(m => m.SendAt).TypeConverter<DateTimeConverter>().Name("Date");
+        Map(m => m.Content).Name("Content");
+        Map(m => m.Attachments).Name("Attachments");
+    }
+}
+
+public class DateTimeConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (text.Contains("AM") || text.Contains("PM")) //28-Apr-22 07:25 PM
+            return DateTime.ParseExact(text, "dd-MMM-yy hh:mm:ss tt", CultureInfo.InvariantCulture.DateTimeFormat);
+        return DateTime.ParseExact(text, "dd-MMM-yy HH:mm:ss", CultureInfo.InvariantCulture.DateTimeFormat);
+    }
+
+    public enum month
+    {
+        Jan = 1,
+        Feb = 2,
+        Mar = 3,
+        Apr = 4,
+        May = 5,
+        Jun = 6,
+        Jul = 7,
+        Aug = 8,
+        Sep = 9,
+        Oct = 10,
+        Nov = 11,
+        Dec = 12
+    }
+}
---
+++ CountingDiffCheck/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using CountingDiffCheck.Model;
+
+Dictionary<long, List<User>> nameHistory = new();
+
+var file = Path.Join(Environment.CurrentDirectory, "count_prev.csv");
+var newFile = Path.Join(Environment.CurrentDirectory, "count_now.csv");
+
+List<Message> GetMessages(string path)
+{
+    using var reader = new StreamReader(path);
+    using var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture);
+    csv.Context.RegisterClassMap<MessageMapper>();
+    return csv.GetRecords<Message>().ToList();
+}
+
+var oldMessages = GetMessages(file);
+var newMessages = GetMessages(newFile);
+
+var oldUsers = oldMessages.Select(message => message.Sender).Distinct();
+var newUsers = newMessages.Select(message => message.Sender).Distinct();
+
+foreach (var user in oldUsers)
+{
+    if (!nameHistory.ContainsKey(user.UserID))
+        nameHistory.Add(user.UserID, new()
+        {
+            user
+        });
+}
+
+foreach (var user in newUsers)
+{
+    if (nameHistory.ContainsKey(user.UserID))
+    {
+        var prev = nameHistory[user.UserID];
+        bool rename = true;
+        foreach (var historyName in prev)
+        {
+            if (Equals(historyName.UserName, user.UserName))
+                rename = false;
+        }
+        if (rename)
+            nameHistory[user.UserID].Add(user);
+    }
+}
+
+foreach (var name in nameHistory)
+{
+    Console.WriteLine($"{name.Key}: {string.Join(", ", name.Value.Select(user => user.UserName))}");
+}
